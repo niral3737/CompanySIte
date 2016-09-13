5a98e14f4e0fa21f591b772db919cc6cc1fade4b
niral.eagle100@gmail.com <niral.eagle100@gmail.com>
2016-09-13 22:02:36 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.NODE_PORT || 3000;
 var app = express();
 
 app.use(express.static('public'));
+
+app.get('/health', function (req, res) {
+  res.status(200);
+});
 
 app.listen(PORT, function () {
   console.log(`Express app is up and running on ${PORT}`);
